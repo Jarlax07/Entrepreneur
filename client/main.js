fb350a7173df646953616bfabf52ce030c290081
Julien <julien.courtial@laposte.net>
2018-02-12 17:15:23 +0100

    modifying Proposer template

--- client/main.js
+++ client/main.js
@@ -34,8 +34,7 @@ Template.body.events({
 Template.Proposer.onCreated(function(){
   this.creneau = new ReactiveVar(false);
   this.precis = new ReactiveVar(false);
-  this.nbcreneau = new Array("Coucou","Salut");
-  this.sizetab = new ReactiveVar(1);
+  this.nbcreneau = new ReactiveVar(0);
 });
 
 Template.Proposer.helpers({
@@ -45,15 +44,13 @@ Template.Proposer.helpers({
   precis:function(){
     return Template.instance().precis.get();
   },
-  nbcreneau:function(){
-    return "{{>choixjour}}";
-  }
 });
 
 Template.Proposer.events({
   'click .creneau': function(event, instance){
     instance.creneau.set(true);
     instance.precis.set(false);
+    instance.nbcreneau.set(1);
     document.getElementById("choix1").checked = true;
     document.getElementById("choix2").checked = false;
   },
@@ -63,4 +60,28 @@ Template.Proposer.events({
     document.getElementById("choix1").checked = false;
     document.getElementById("choix2").checked = true;
   },
+  'click .add':function(event,instance){
+    instance.nbcreneau.set(instance.nbcreneau.get()+1);
+    var field = "<br>Le <select name='creneau"+instance.nbcreneau.get()+"' size='1'><option>Lundi</option><option>Mardi</option><option>Mercredi</option><option>Jeudi</option><option>Vendredi</option><option>Samedi</option><option>Dimanche</option></select> de <input type='time' name='starthour'> à <input type='time' name='endhour'>";
+    instance.find("#newcreneau").innerHTML += field;
+  },
 });
+
+Template.precisedatechoice.helpers({
+  today:function(){
+    var date = new Date();
+    var dd = date.getDate();
+    var mm = date.getMonth()+1;
+    var yyyy = date.getFullYear();
+
+    if(dd<10) {
+      dd = '0'+dd
+    }
+
+    if(mm<10) {
+      mm = '0'+mm
+    }
+
+    return yyyy + '-' + mm + '-' + dd;
+  }
+})
