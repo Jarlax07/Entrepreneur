2357f89e3db7ec683b5a0e1f1464a6212b059bf2
Julien <julien.courtial@laposte.net>
2018-02-26 17:32:02 +0100

    adding account manage

--- client/main.js
+++ client/main.js
@@ -1,27 +1,23 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Session } from 'meteor/session';
-
+import { Accounts } from 'meteor/accounts-base';
+import { Mongo } from 'meteor/mongo';
 import './main.html';
 
 Meteor.startup(function(){
-    Session.setDefault("template_name", "Accueil");
+  PlayersList = new Mongo.Collection('players');
+
 });
 
 Template.body.helpers({
-  template_name : function(){
-    return  Session.get("template_name")
+  username :function(){
+    return PlayersList.find();
   }
 });
 
 
 Template.body.events({
-  "click .gestion":function(event,instance){
-    Session.set("template_name","Gestion")
-  },
-  "click .accueil":function(event,instance){
-    Session.set("template_name","Accueil");
-  },
 });
 
 
@@ -33,4 +29,10 @@ Router.route('/Proposer');
 Router.route('/Gestion');
 
 // Checker si la personne est connecté sinon lui demander de se connecter ou s'inscrire
-Router.route('/Connexion');
+Router.route('/Connexion',function(){
+  if(Meteor.userId()){
+    this.render('Gestion');
+  }else{
+    this.render('Connexion');
+  }
+});
--- server/main.js
+++ server/main.js
@@ -2,4 +2,9 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  PlayersList = new Mongo.Collection("players");
+
+  PlayersList.insert({name:"david"});
+  PlayersList.insert({name:"julien"});
+  PlayersList.insert({name:"vincent"});
 });
